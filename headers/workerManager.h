03c3b6f7ba9b60ccc16b8434c6ffc9d1aa020d1b
garfield-awa <basketball616@163.com>
2025-02-25 21:37:15 +0800

    要提交的变更： 	新文件：   .gitignore 	新文件：   .vscode/c_cpp_properties.json 	新文件：   .vscode/launch.json 	新文件：   .vscode/settings.json 	修改：     .vscode/tasks.json 	新文件：   CMakeLists.txt 	新文件：   bin/management_system 	新文件：   headers/workerManager.h 	修改：     sources/management_system 	修改：     sources/management_system.cpp 	新文件：   sources/workerManager.cpp 	新文件：   user_manual.txt

---
+++ headers/workerManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class WorkerManager
+{
+    private:
+        /* data */
+    public:
+        WorkerManager();
+
+        /*展示菜单*/
+        void showMenu();
+
+        void exitSystem();
+
+        void pause();
+
+        ~WorkerManager();
+};
+
+
--- sources/management_system.cpp
+++ sources/management_system.cpp
@@ -1,5 +1,30 @@
-#include<iostream>
+#include <iostream>
+#include "workerManager.h"
 using namespace std;
+
 int main(){
-    cout << "test" << endl;
+    WorkerManager vm;
+    vm.showMenu();
+    int choice;
+    
+    while(true){
+        cout << "请输入选择" << endl;
+        cin >> choice;
+        switch(choice){
+            case 0:
+                vm.exitSystem();
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
 }
